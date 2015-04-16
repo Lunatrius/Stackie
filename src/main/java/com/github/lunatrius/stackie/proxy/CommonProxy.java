0d3942f421226c150b4bc8a0fbc1563a49b7ac5e
Lunatrius <jadran.kotnik@gmail.com>
2015-04-17 00:47:17 +0200

    Changed the entity matching to require exact classes (should fix some item disappearing issues). Changed the stacking order, keeping new entities and removing old entities. Split the items and experience orbs into separate lists for performance.

--- src/main/java/com/github/lunatrius/stackie/proxy/CommonProxy.java
+++ src/main/java/com/github/lunatrius/stackie/proxy/CommonProxy.java
@@ -1,30 +1,27 @@
 package com.github.lunatrius.stackie.proxy;
 
 import com.github.lunatrius.stackie.command.StackieCommand;
-import com.github.lunatrius.stackie.handler.SpawnHandler;
-import com.github.lunatrius.stackie.handler.Ticker;
+import com.github.lunatrius.stackie.handler.StackingHandlerJoin;
+import com.github.lunatrius.stackie.handler.StackingHandlerTick;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.event.FMLServerStoppingEvent;
 import net.minecraftforge.common.MinecraftForge;
 
 public abstract class CommonProxy {
-    private Ticker ticker = null;
-
     public abstract void setConfigEntryClasses();
 
     public void registerEvents() {
-        this.ticker = new Ticker();
-        FMLCommonHandler.instance().bus().register(this.ticker);
-        MinecraftForge.EVENT_BUS.register(new SpawnHandler());
+        FMLCommonHandler.instance().bus().register(StackingHandlerTick.INSTANCE);
+        MinecraftForge.EVENT_BUS.register(new StackingHandlerJoin());
     }
 
     public void serverStarting(FMLServerStartingEvent event) {
-        this.ticker.setServer(event.getServer());
+        StackingHandlerTick.INSTANCE.setServer(event.getServer());
         event.registerServerCommand(new StackieCommand());
     }
 
     public void serverStopping(FMLServerStoppingEvent event) {
-        this.ticker.setServer(null);
+        StackingHandlerTick.INSTANCE.setServer(null);
     }
 }
